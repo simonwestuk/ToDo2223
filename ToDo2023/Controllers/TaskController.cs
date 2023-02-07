d1e073552d7779691a7ce41cf4d4e2f439e157bf
wests <wests@C-010053>
2023-02-07 16:19:54 +0000

    Added Task

---
+++ ToDo2023/Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo2023.Models;
+
+namespace ToDo2023.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        } 
+        
+        [HttpPost]
+        public IActionResult Add(TaskModel task)
+        {
+            return View();
+        }
+    }
+}
---
+++ ToDo2023/Models/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo2023.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        [Required]
+        public bool Completed { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DueDate { get; set; }
+    }
+}
